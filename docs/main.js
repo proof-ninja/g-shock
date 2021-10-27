e52e06e504f4f36b29e206a60ad93de559605cb8
Gamishi0704 <cjh14133@nitech.jp>
2021-10-28 01:40:56 +0900

    グラフが出るようにしました

--- docs/main.js
+++ docs/main.js
@@ -10,3 +10,30 @@ fetch(endpoint)
     const object = data;
     console.log(object);
   })
+// === include 'setup' then 'config' above ===
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'VIX Chart',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 155, 5, 2, 20, 30, 45],
+  }]
+};
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+  );
