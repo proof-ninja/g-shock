3355172379ff9ab8150b411a33dd67a9128618f2
Gamishi0704 <cjh14133@nitech.jp>
2021-10-28 01:51:45 +0900

    グラフが出るようにしました

--- docs/main.js
+++ docs/main.js
@@ -9,31 +9,33 @@ fetch(endpoint)
     //JSONから配列に変換
     const object = data;
     console.log(object);
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'VIX Chart',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [object.date[0], 155, 5, 2, 20, 30, 45],
+      }]
+    };
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+    const myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+      );
   })
 // === include 'setup' then 'config' above ===
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'VIX Chart',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [object.date[0], 155, 5, 2, 20, 30, 45],
-  }]
-};
-const config = {
-  type: 'line',
-  data: data,
-  options: {}
-};
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-  );
+
+
