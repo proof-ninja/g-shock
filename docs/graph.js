fa8bb030bea2a7698d572bbf74f0dbeb054e9afa
Gamishi0704 <cjh14133@nitech.jp>
2021-10-28 01:23:28 +0900

    グラフが出るようにしました

---
+++ docs/graph.js
@@ -0,0 +1,27 @@
+// === include 'setup' then 'config' above ===
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 155, 5, 2, 20, 30, 45],
+  }]
+};
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+  );
