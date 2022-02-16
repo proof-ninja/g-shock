e70425d82bf689395b034cdffeab13c9e7aa33af
Gamishi0704 <cjh14133@nitech.jp>
2022-02-16 23:59:10 +0900

    プルダウン試行しました3

--- docs/main.js
+++ docs/main.js
@@ -3,48 +3,49 @@ function fetchVix() {
   return fetch(url);
 }
 
+function buttonClick(){
+  if(timesSelect == fiveyears){function showGraph(label, element_id, data) {
+    //JSONから配列に変換
+    const object = data;
+    console.log(object);
+    console.log(object[0].Date);
+    const labels = [];
+    for (let i = 0; i < object.length; i++) {
+      labels[i] = object[i].Date;
+    }
+    const data1 = [];
+    for (let i = 0; i < object.length; i++) {
+      data1[i] = object[i].Close;
+    }
+    console.log(labels);
+    console.log(data1);
+    const data2 = {
+      labels: labels,
+      datasets: [{
+        label: label,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: data1,
+      }]
+    };
+    console.log(data2);
+    const config = {
+      type: 'line',
+      data: data2,
+      options: {}
+    };
+    const myChart1 = new Chart(
+      document.getElementById(element_id),
+      config);
+    return;
+  };}
+}
+
 let timesSelect = document.getElementById('times');
 timesSelect.options[2].selected = true;
 
 let checkButton = document.getElementById('checkButton');
 checkButton.addEventListener('click', buttonClick);
-
-if(timesSelect == fiveyears){function showGraph(label, element_id, data) {
-  //JSONから配列に変換
-  const object = data;
-  console.log(object);
-  console.log(object[0].Date);
-  const labels = [];
-  for (let i = 0; i < object.length; i++) {
-    labels[i] = object[i].Date;
-  }
-  const data1 = [];
-  for (let i = 0; i < object.length; i++) {
-    data1[i] = object[i].Close;
-  }
-  console.log(labels);
-  console.log(data1);
-  const data2 = {
-    labels: labels,
-    datasets: [{
-      label: label,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: data1,
-    }]
-  };
-  console.log(data2);
-  const config = {
-    type: 'line',
-    data: data2,
-    options: {}
-  };
-  const myChart1 = new Chart(
-    document.getElementById(element_id),
-    config);
-  return;
-};}
-
 
 function fetchVXN() {
   const url = "https://script.google.com/macros/s/AKfycbzUZAdeaXrxmJ-ziBIEFXHx8Nom0Sri3FjTCqqrRkbrkMJnAZdj0qgMSzWzHD8BH0mLVA/exec?ticker=VXN";
