7d63ac2ecc75e77eb4fb9534ac1e5311ddec61bd
Gamishi0704 <cjh14133@nitech.jp>
2022-03-03 01:37:39 +0900

    1年表記にしました

--- docs/main.js
+++ docs/main.js
@@ -44,7 +44,7 @@ function buttonClick() {
     fetchVix()
         .then(response => response.json())
         .then(data => {
-          showGraph("VIX Chart", "myChart1", data,4116);
+          showGraph("VIX Chart", "myChart1", data,4429);
         });
   }
 }
